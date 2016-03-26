a2369e9f28e362cb3604050ac644ee361554fe75
walterov <walterov@gmail.com>
2016-03-25 21:22:09 -0700

    fixed comma
    fixed comma

--- mesos-marathon/checkjson.ps1
+++ mesos-marathon/checkjson.ps1
@@ -1,3 +1,3 @@
-﻿$path = "C:\Users\waltero\OneDrive\Azure Stuff\scratch\mesos-marathon\wos-mesos-cluster-with-windows-jumpbox.json"
+﻿$path = "C:\Users\waltero\OneDrive\Azure Stuff\scratch\mesos-marathon\wos0-mesos-cluster-with-windows-jumpbox.json"
 $content = Get-Content -path $path -Raw
 $content | ConvertFrom-Json
